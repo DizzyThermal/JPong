3e29456537d447ab1711e4e717f6823f2d708c28
kblanch09 <kblanch09@gmail.com>
2013-04-24 19:55:02 -0400

    New Version Eggplant

--- src/Resource.java
+++ src/Resource.java
@@ -1,8 +1,8 @@
 public class Resource
 {
-	public static final String VERSION_NUMBER		= "v0.0.4";
-	public static final String VERSION_CODENAME		= "Dingleberry";
+	public static final String VERSION_NUMBER		= "v0.0.5";
+	public static final String VERSION_CODENAME		= "Eggplant";
 	
-	public static String IP							= "192.168.1.103";
+	public static String IP							= "192.168.1.100";
 	public static String PORT						= "8015";
 }
